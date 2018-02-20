61000a79f0aa3a74321cacd952b7902a5aa473d9
najayama <27891506+najayama@users.noreply.github.com>
2018-02-21 07:29:54 +0900

    fix dependency

--- Sound_to_vector.py
+++ Sound_to_vector.py
@@ -1,16 +1,9 @@
+#サードパーティライブラリ
 import os
+
+#下のコメントセンスない気がする
+#その他のライブラリ
+import 
 
 #設定ファイルの読み込み。
 
-os.chdir("config_files")
-
-with open("config_files/") as config_file:
-    LINE = config_file.read()
-    while LINE:
-        if LINE[0:14] == "DEVIDE_INTERVAL":
-            DEVIDE_INTERVAL = float()
-    
-    
-os.chdir("..")
-    
-    
