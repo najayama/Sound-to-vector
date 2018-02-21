5b9f6b5e170ad63e008fb0c3a3b5c5bdaa112c52
najayama <27891506+najayama@users.noreply.github.com>
2018-02-22 00:19:04 +0900

    add some comment.

--- Sound_to_vector.py
+++ Sound_to_vector.py
@@ -1,9 +1,28 @@
-#サードパーティライブラリ
-import os
+###全体の流れは以下の通り
+#１．設定ファイルの読み込み
+#２．変換すべきファイルのリストの取得
+#３．リストのそれぞれのファイルについて、
+#    {
+#    音声ファイルを区切る
+#    フーリエ変換
+#    結果の書き出し
+#    }
 
-#下のコメントセンスない気がする
-#その他のライブラリ
-import 
+
+import config_files.read_config as read_config
 
 #設定ファイルの読み込み。
+config_dict = read_config.read_config()
+
+devide_interval = config_dict["DEVIDE_INTERVAL"]
+output_dir = config_dict["OUTPUT_DIR"]
+input_dir = config_dict["INPUT_DIR"]
+
+
+#INPUT_DIR 直下の音声ファイルのリストを作成。
+#音声ファイルを区切る
+#フーリエ変換
+#結果の書き出し
+
+
 
