e54b17f8f6b47de49552e95d3594d5bc52d69b4d
najayama <27891506+najayama@users.noreply.github.com>
2018-02-21 23:45:27 +0900

    before test read_config

--- config_files/read_config.py
+++ config_files/read_config.py
@@ -1,39 +1,47 @@
 #設定を読み込むための関数群。
 #辞書形式で設定項目名と値の組を返す。
+import sys
+import os
+
+config_list = ["DEVIDE_INTERVAL", "OUTPUT_DIR", "INPUT_DIR"]
 
 #エラー吐いて終了する関数
 def _print_error(error_str,error_info):
-    import sys
+
     print("エラー：" + error_str % error_info)
     sys.exit()
 
+#必要な項目がすべて辞書に存在するかチェックする関数
+def _check_enough_val(arg_dict):
+    for item in config_list:
+        try:
+            arg_dict[item]
+        except IndexError:
+            _print_error("%sの設定行が見当たりません。\n"
+                             "%s = 値\n"
+                             "の形式で書かれているか確かめてください"
+                            ,(item,  item))
+
 
 #辞書中の数字の変数の値が文字列のままなので数値に直す。（例："3" -> 3）
-def _convert_values(arg_dict):
+def _convert_values(arg_dict):       
     
-    
-    #設定項目が見当たらなかった際のメッセージを出力する関数。
-    def _p_how_to_define(item_name):
-        _print_error("%sの設定行が見当たりません。\n"
-                     "%s = 値\n"
-                     "の形式で書かれているか確かめてください")
-        
-        interval = arg_dict["DEVIDE_INTERVAL"]    
+    interval = arg_dict["DEVIDE_INTERVAL"]           
     try:
         arg_dict["DEVIDE_INTERVAL"] = float(interval)
-        
-    except IndexError:
-        _p_how_to_define("DEVIDE_INTERVAL")
     except ValueError:
         _print_error("DEVIDE_INTERVALに指定された値が異常です。\n"
                     "指定された値：%s", (interval))
-        
-
+    
+    return arg_dict
 
 #辞書に格納した変数が正常な値かチェックする。
 def _check_values(arg_dict):
-    pass
-
+    for path in ["INPUT_DIR", "OUTPUT_DIR"]:
+        if not os.path.isdir(arg_dict[path]):
+            _print_error("%sで指定されたpath\n%s\nはディレクトリではありません"
+                         ,(path, arg_dict[path]))
+    
 
 def read_config():
     config_dictionary = {}
@@ -49,7 +57,7 @@ def read_config():
             line = line.strip()
 
             #コメント行は無視
-            if(line == "" or line[0] = "#"):
+            if(line == "" or line[0] == "#"):
                 pass
             else:
                 try:
@@ -67,8 +75,8 @@ def read_config():
             current_line += 1
             line = confile.readline()
         
-    config_dictionary = _convert_values(config_dictionary)
-    config_dictionary = _check_values(config_dictionary)
+    _check_enough_val(config_dictionary)
+    _check_values(config_dictionary)
     
     return config_dictionary
 
